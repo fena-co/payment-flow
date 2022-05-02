96de3f650600730d012e228d2379f641ffab8700
alex_t <alex@fena.co>
2022-05-02 14:35:43 +0400

    feat: hardcoded api url (for now)

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-const apiUrl = process.env.API_URL || `http://localhost:8084`;
+const apiUrl = process.env.API_URL || `https://api.stage.fena.co/`;
 
 class Api {
   private readonly apiUrl: string;
