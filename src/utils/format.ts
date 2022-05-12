69c67c7d055036bb2c2932ea73f1474ea58f6a88
alex_t <alex@fena.co>
2022-05-12 15:40:40 +0400

    fix: amount format

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -3,9 +3,15 @@ export const formatSortCode = (sortCode: string) =>
 
 export const formatAmount = (amount: string) => {
   let result = ``;
-  if (amount.search(`.`) !== -1) {
+  if (amount.indexOf(`.`) !== -1) {
     const split = amount.split(`.`);
-    if (split[1].length < 2) result = `${split[2]}0`;
+    if (split[1].length < 2) {
+      result = `${split[0]}.${split[1]}0`;
+    } else if (split[1].length > 2) {
+      result = `${split[0]}.${split[1].slice(0, 2)}`;
+    } else {
+      result = amount;
+    }
   } else {
     result = `${amount}.00`;
   }
