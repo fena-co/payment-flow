a5df7b883cd1821956dc1bb08e7d28963c1d7c26
Alex I <alex@fena.co>
2022-06-06 20:57:41 +0400

    fix: env vars

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,5 @@
-const apiUrl = process.env.GATSBY_API_URL || `https://api.stage.fena.co`;
+const apiUrl =
+  process.env.GATSBY_API_URL || `https://business.api.staging.fena.co`;
 
 class Api {
   private readonly apiUrl: string;
